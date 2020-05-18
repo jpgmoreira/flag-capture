18ddc4588e625c4e4391f6c8314cc12637a66f54
JoaoPedro <jpgmoreira19@gmail.com>
2020-05-18 18:07:31 -0300

    Revised src. To be revised: public/js.

--- controller/src/app.js
+++ controller/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const pubDir = path.join(__dirname, '../public');
 
 app.use(express.static(pubDir));
